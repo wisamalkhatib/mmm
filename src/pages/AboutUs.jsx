bc4dbdc78f1fbf52b449db464584a6e0bf4e93b8
wisam alkhatib <migrando.dev1@gmail.com>
2025-06-23 16:13:56 +0200

    auto-commit

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company! We are a dedicated team of professionals committed to 
+        delivering high-quality products and services to our customers.
+      </p>
+      <p>
+        Founded in 2025, our mission is to innovate and create solutions that make 
+        a positive impact on people's lives. We believe in collaboration, creativity, 
+        and continuous improvement.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
